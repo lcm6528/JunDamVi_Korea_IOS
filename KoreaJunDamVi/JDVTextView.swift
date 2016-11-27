52eb064026b7c531aea11f3ee2f9994297df4bbe
Changmin <lcmini6528@gmail.com>
2016-11-27 22:45:39 +0900

    probtestinnerVC Choices layout

---
+++ KoreaJunDamVi/JDVTextView.swift
@@ -0,0 +1,21 @@
+//
+//  JDVTextView.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 11. 27..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+class JDVTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
