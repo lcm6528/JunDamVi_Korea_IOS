1b66cb05728be1e417a2c574c2c325bc026b7ba4
Changmin <lcmini6528@gmail.com>
2016-12-12 19:33:09 +0900

    add ProbResultSubViews

---
+++ KoreaJunDamVi/ProbResultTopView.swift
@@ -0,0 +1,56 @@
+//
+//  ProbResultTopView.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 12. 12..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ProbResultTopView: UIView {
+  var view:UIView!
+  let NibName:String = "ProbResultTopView"
+  
+  var delegate:ProbResultSubViewDelegate?
+  
+  
+  override init(frame: CGRect) {
+    
+    super.init(frame: frame)
+    setup()
+    
+  }
+  
+  
+  required init(coder aDecoder: NSCoder) {
+    
+    super.init(coder: aDecoder)!
+    setup()
+  }
+  
+  func setup() {
+    
+    view = loadViewFromNib()
+    view.frame = bounds
+    view.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+    addSubview(view)
+    
+  }
+  
+  
+  func loadViewFromNib() -> UIView {
+    let bundle = Bundle(for:type(of: self))
+    let nib = UINib(nibName:NibName, bundle: bundle)
+    let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+    
+    return view
+  }
+  
+  @IBAction func changeView(_ sender: AnyObject) {
+    self.delegate?.changeView()
+  }
+  
+}
---
+++ KoreaJunDamVi/ProbResultViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ProbResultViewController.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 12. 12..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+protocol ProbResultSubViewDelegate {
+  func changeView()
+}
+
+class ProbResultViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,ProbResultSubViewDelegate {
+  
+  
+  @IBOutlet var scrollView: UIScrollView!
+  @IBOutlet var contentView: UIView!
+  
+  var heightOfSubView:CGFloat!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    heightOfSubView = self.view.frame.size.height-getHeightOfStatusNNaviBar(self)
+    let topView = ProbResultTopView(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: heightOfSubView))
+    topView.delegate = self
+    
+    let botView = ProbResultBotView(frame: CGRect(x: 0, y: heightOfSubView, width: SCREEN_WIDTH, height: heightOfSubView))
+    botView.delegate = self
+    botView.tableView.delegate = self
+    botView.tableView.dataSource = self
+    
+    contentView.addSubview(topView)
+    contentView.addSubview(botView)
+    
+    
+    
+    
+    
+  }
+  
+  
+
+  
+  func changeView(){
+    
+    let offsetY:CGFloat = scrollView.contentOffset.y == 0.0 ? heightOfSubView : 0.0
+    
+    scrollView.setContentOffset(CGPoint(x: 0, y: offsetY) , animated: true)
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    return 20
+  }
+  
+  
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+    if let cell = tableView.dequeueReusableCell(withIdentifier: "cell"){
+      cell.textLabel?.text = "test"
+      return cell
+    }else{
+      let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+      cell.textLabel?.text = "test"
+      return cell
+    }
+  }
+  
+  
+}
