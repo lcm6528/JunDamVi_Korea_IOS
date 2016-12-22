fc8d8e608ad3353fe3a4b51325ddec2a00c10e0d
Changmin <lcmini6528@gmail.com>
2016-12-22 16:51:00 +0900

    Import JDVColors

---
+++ KoreaJunDamVi/JDVColors.swift
@@ -0,0 +1,24 @@
+//
+//  JDVColors.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 12. 22..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+  class var untBlack505050: UIColor {
+    return UIColor(white: 50.0 / 255.0, alpha: 1.0)
+  }
+  
+  class var untPaleRed: UIColor {
+    return UIColor(red: 225.0 / 255.0, green: 64.0 / 255.0, blue: 64.0 / 255.0, alpha: 1.0)
+  }
+  
+  class var untBlack757575: UIColor {
+    return UIColor(white: 75.0 / 255.0, alpha: 1.0)
+  }
+}
