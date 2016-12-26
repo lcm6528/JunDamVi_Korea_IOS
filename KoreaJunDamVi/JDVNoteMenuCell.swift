45a40b0d4c6d63108f14a2aadfc3e5da05afa328
Changmin <lcmini6528@gmail.com>
2016-12-26 22:51:15 +0900

    import NoteVC

---
+++ KoreaJunDamVi/JDVNoteMenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  JDVNoteMenuCell.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 12. 26..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+class JDVNoteMenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
