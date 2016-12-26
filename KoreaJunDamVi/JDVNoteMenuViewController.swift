307e459294dd6a11bc2243f4140dd1f85872bf4d
Changmin <lcmini6528@gmail.com>
2016-12-26 22:53:23 +0900

    import tableView

--- KoreaJunDamVi/JDVNoteMenuViewController.swift
+++ KoreaJunDamVi/JDVNoteMenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class JDVNoteMenuViewController: UIViewController {
+class JDVNoteMenuViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,19 +17,23 @@ class JDVNoteMenuViewController: UIViewController {
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+       super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+extension JDVNoteMenuViewController: UITableViewDelegate,UITableViewDataSource{
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! JDVNoteMenuCell
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  
+  
 }
