e81c5cdb56dc048e15873bda5fb04162738ec1b8
Changmin <lcmini6528@gmail.com>
2016-12-27 16:30:45 +0900

    import JDVGraph

---
+++ KoreaJunDamVi/JDVGraph.swift
@@ -0,0 +1,21 @@
+//
+//  JDVGraph.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 12. 27..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+class JDVGraph: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ KoreaJunDamVi/JDVGraphCell.swift
@@ -0,0 +1,21 @@
+//
+//  JDVGraphCell.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2016. 12. 27..
+//  Copyright © 2016년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+class JDVGraphCell: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
