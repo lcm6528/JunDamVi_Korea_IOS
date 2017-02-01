7ce7c1680d27a1db4746eb3a5ee7f74ef77433e2
Changmin <lcmini6528@gmail.com>
2017-02-01 16:25:30 +0900

    BlockView 추가

---
+++ KoreaJunDamVi/BlockView.swift
@@ -0,0 +1,21 @@
+//
+//  BlockView.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2017. 2. 1..
+//  Copyright © 2017년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+class BlockView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
