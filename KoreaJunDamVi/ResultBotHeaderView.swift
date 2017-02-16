f824177f2931234e3e7036d8c83270d1f7914ab5
Changmin <lcmini6528@gmail.com>
2017-02-16 16:31:11 +0900

    rename

---
+++ KoreaJunDamVi/ResultBotHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultBotHeaderView.swift
+//  KoreaJunDamVi
+//
+//  Created by 이창민 on 2017. 2. 16..
+//  Copyright © 2017년 JunDamVi. All rights reserved.
+//
+
+import UIKit
+
+class ResultBotHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
