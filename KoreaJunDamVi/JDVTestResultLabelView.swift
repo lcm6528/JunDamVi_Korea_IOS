b84557d71a400a680e18e04dc51994bcf25a012d
Changmin <lcmini6528@gmail.com>
2017-02-26 23:08:23 +0900

    Edit indent

--- KoreaJunDamVi/JDVTestResultLabelView.swift
+++ KoreaJunDamVi/JDVTestResultLabelView.swift
@@ -33,7 +33,6 @@ import UIKit
     }
   }
   
-  
   override init(frame: CGRect) {
     
     super.init(frame: frame)
@@ -41,33 +40,30 @@ import UIKit
     
   }
   
-  
   required init(coder aDecoder: NSCoder) {
     
     super.init(coder: aDecoder)!
     setup()
+    
   }
   
   func setup() {
+    
     view = loadViewFromNib()
     view.frame = bounds
     view.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
     addSubview(view)
+    
   }
   
   func loadViewFromNib() -> UIView {
+    
     let bundle = Bundle(for:type(of: self))
     let nib = UINib(nibName:"JDVTestResultLabelView", bundle: bundle)
     let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
     
     return view
+    
   }
-  
-  
-  
-  
-  
-  
-  
   
 }
