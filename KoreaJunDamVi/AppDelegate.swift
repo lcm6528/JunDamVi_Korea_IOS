ce1192f029a71f201686039d96c32c16e34ebda1
changmin lee <lcmini6528@gmail.com>
2017-03-06 16:11:54 +0900

    Print Document Path

--- KoreaJunDamVi/AppDelegate.swift
+++ KoreaJunDamVi/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    print(documentPath)
     return true
   }
 
