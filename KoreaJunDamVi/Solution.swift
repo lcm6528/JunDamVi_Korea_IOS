dbdb29e341d5dda1c9ae2eb52c74746e8fabd46a
changmin lee <lcmini6528@gmail.com>
2017-03-21 16:09:35 +0900

    AttrString 처리

--- KoreaJunDamVi/Solution.swift
+++ KoreaJunDamVi/Solution.swift
@@ -7,3 +7,82 @@
 //
 
 import Foundation
+import UIKit
+import EZSwiftExtensions
+
+struct Solution {
+    var ProbID:Int
+    var TestNum:Int
+    var ProbNum:Int
+    
+    var keyword_String:String
+    var keyword_attString: NSAttributedString?{
+        return setToAttr(from: keyword_String)
+    }
+    
+    var content1_String:String
+    var content1_attString:NSAttributedString?{
+        return setToAttrWithImage(from: content1_String)
+    }
+    
+    var content2_String:String
+    var content2_attString:NSAttributedString?{
+        return setToAttr(from: content2_String)
+    }
+    
+    
+    init(withDict dict:NSDictionary){
+        
+        self.ProbID = dict["probId"] as! Int
+        self.TestNum = dict["testnum"] as! Int
+        self.ProbNum = dict["probnum"] as! Int
+    
+        
+        self.keyword_String = dict["keyword"] as! String
+        self.content1_String = dict["content1"] as! String
+        self.content2_String = dict["content2"] as! String
+        
+        
+    }
+    
+    func setToAttr(from str:String)->NSAttributedString{
+        
+        let result = NSMutableAttributedString()
+        result.append(stringToAttrStringInHTML(str))
+        result.addAttribute(NSFontAttributeName, value: UIFont.solutionFont, range: NSRange(location: 0, length: result.length))
+        return result
+        
+    }
+    
+    func setToAttrWithImage(from str:String)->NSAttributedString{
+        
+        let result = replaceTagToImage(withString: str, imgName: "sol_\(ProbID)")
+        result.addAttribute(NSFontAttributeName, value: UIFont.solutionFont, range: NSRange(location: 0, length: result.length))
+        return result
+
+        
+    }
+    
+    
+    func replaceTagToImage(withString str:String, imgName name:String, withWidth width:CGFloat = SCREEN_WIDTH - 20)->NSMutableAttributedString{
+        
+        let result = NSMutableAttributedString()
+        
+        if let bundlePath = Bundle.main.path(forResource: name, ofType: "png"){
+            
+            let attachIcon:NSTextAttachment = NSTextAttachment()
+            attachIcon.image = UIImage(contentsOfFile: bundlePath)
+            let scaleFactor = attachIcon.image!.size.width / (width)
+            attachIcon.image = UIImage(cgImage: attachIcon.image!.cgImage!, scale: scaleFactor, orientation: UIImageOrientation.up)
+            let imageString = NSAttributedString(attachment: attachIcon)
+            result.append(imageString)
+            result.append(NSAttributedString(string: "\n"))
+            
+        }
+        
+        result.append(stringToAttrStringInHTML(str))
+        return result
+        
+    }
+    
+}
