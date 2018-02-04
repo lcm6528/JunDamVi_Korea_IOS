1d0fbe972d66ac7870908ea3643a5aab2ae5aff9
changmin lee <lcmini6528@gmail.com>
2018-02-05 01:50:09 +0900

    컨벤션 수정

--- KoreaJunDamVi/HomeViewController.swift
+++ KoreaJunDamVi/HomeViewController.swift
@@ -12,31 +12,26 @@ class HomeViewController: JDVViewController {
     
     @IBOutlet var roundView1: RoundView!
     @IBOutlet var centerContainerView: UIView!
-    
     @IBOutlet var label_MidView: UILabel!
-    
     @IBOutlet var ddayTitleLabel: UILabel!
     @IBOutlet var ddayCotentLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     
-    
     var Tests:[String] = []
     var completeCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setTitleWithStyle("한국사 비법노트")
-        
         self.centerContainerView.layer.cornerRadius = 5.0
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
         fetchList()
         setDday()
         label_MidView.attributedText = getAttrStr(totalValue: Tests.count , withValue: completeCount)
         
         roundView1.setValue(value: CGFloat(completeCount)/CGFloat(Tests.count)*100, animate: false)
-        
     }
   
     func fetchList() {
@@ -49,32 +44,29 @@ class HomeViewController: JDVViewController {
             dict = object as! NSDictionary
         } catch {}
         
-        Tests = dict.value(forKey:JDVProbManager.SortedOption.test.rawValue) as!  [String]
-        
+        Tests = dict.value(forKey:JDVProbManager.SortedOption.test.rawValue) as! [String]
         completeCount = 0
         
-        for test in Tests{
+        for test in Tests {
             if let arr = JDVProbManager.getCachedData(with: test) {
-                if arr.isEmpty == true{
+                if arr.isEmpty == true {
                     completeCount += 1
                 }
-                
             }
         }
     }
     
     func setDday() {
-        
         let jsondata = try! Data(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "testday", ofType: "json")!))
         let json = JSON(data:jsondata)["data"]
         let arr = json.arrayObject as! [[String:String]]
         
-        for item in arr{
+        for item in arr {
             let test = item.first!
             let date = Date(dateString: item.first!.value)
             let now = Date()
-            if date < now{continue}
-            else if date > now{
+            if date < now { continue }
+            else if date > now {
                 let formatter = DateFormatter()
                 formatter.dateFormat = "yyyy. MM. dd"
                 
@@ -82,28 +74,23 @@ class HomeViewController: JDVViewController {
                 ddayTitleLabel.text = "\(test.key)회 한국사능력검정시험"
                 ddayCotentLabel.text = "D\(dday)"
                 dateLabel.text = formatter.string(from: date)
-                
                 break
             }
         }
-        
     }
     
-    func getAttrStr(totalValue total:Int,withValue value:Int)->NSMutableAttributedString{
-        
+    func getAttrStr(totalValue total: Int, withValue value: Int)->NSMutableAttributedString {
         let font1 = UIFont(name: "NanumBarunGothic", size: 15)!
         let font2 = UIFont(name: "NanumBarunGothicUltraLight", size: 15)!
-        
-        
         let str1 = NSMutableAttributedString(string: "총 \(total)개")
         let str2 = NSMutableAttributedString(string: "의 기출 문제 중 ")
         let str3 = NSMutableAttributedString(string: "\(value)개 ")
         let str4 = NSMutableAttributedString(string: "풀이 진행")
         
-        str1.addAttributes([NSAttributedStringKey.font: font1, NSAttributedStringKey.foregroundColor: UIColor.untBlack757575], range: NSRange(location: 0, length: str1.length) )
-        str2.addAttributes([NSAttributedStringKey.font: font2, NSAttributedStringKey.foregroundColor: UIColor.untBlack505050], range: NSRange(location: 0, length: str2.length) )
-        str3.addAttributes([NSAttributedStringKey.font: font1, NSAttributedStringKey.foregroundColor: UIColor.untPaleRed], range: NSRange(location: 0, length: str3.length) )
-        str4.addAttributes([NSAttributedStringKey.font: font2, NSAttributedStringKey.foregroundColor: UIColor.untBlack505050], range: NSRange(location: 0, length: str4.length) )
+        str1.addAttributes([NSAttributedStringKey.font: font1, NSAttributedStringKey.foregroundColor: UIColor.untBlack757575], range: NSRange(location: 0, length: str1.length))
+        str2.addAttributes([NSAttributedStringKey.font: font2, NSAttributedStringKey.foregroundColor: UIColor.untBlack505050], range: NSRange(location: 0, length: str2.length))
+        str3.addAttributes([NSAttributedStringKey.font: font1, NSAttributedStringKey.foregroundColor: UIColor.untPaleRed], range: NSRange(location: 0, length: str3.length))
+        str4.addAttributes([NSAttributedStringKey.font: font2, NSAttributedStringKey.foregroundColor: UIColor.untBlack505050], range: NSRange(location: 0, length: str4.length))
         
         let str = NSMutableAttributedString()
         str.append(str1)
@@ -111,7 +98,6 @@ class HomeViewController: JDVViewController {
         str.append(str3)
         str.append(str4)
         return str
-        
     }
     
     @IBAction func settingButtonPressed(_ sender: AnyObject) {
@@ -125,7 +111,5 @@ class HomeViewController: JDVViewController {
         default :
             return
         }
-        
     }
 }
-
